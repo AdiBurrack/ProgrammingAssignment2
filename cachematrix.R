aa6b01cc983133f85d03117448504855771eff21
AdiB <adiduush88@gmail.com>
2015-05-24 00:10:10 +0300

    updated task functions

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,35 @@
-## Put comments here that give an overall description of what your
-## functions do
+#The following functions calculates the inverse of a matrix. 
+#It tries to decrease computation time by checking if the inverse has allredy been calculated,
+#if so it skips the computation time.
 
-## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
+  #return set/get functions for cached matrix
+  m <- NULL
+  set <- function(y) {
+    x <<- y
+    m <<- NULL
+  }
+  get <- function() x
+  setinvM <- function(invM) m <<- invM
+  getinvM <- function() m
+  list(set = set, get = get,
+       setinvM = setinvM,
+       getinvM = getinvM)
 
 }
 
 
-## Write a short comment describing this function
-
+# returns the inv of a matrix - using cache data to shorten the runtime.
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+  m <- x$getinvM()
+  if(!is.null(m)) { #if this is not the first time we calculate the inv
+    message("getting cached data")
+    return(m)
+  } #if this is the first time we calculate the inv 
+  data <- x$get()
+  m <- solve(data, ...)
+  x$setinvM(m)
+  m
 }
